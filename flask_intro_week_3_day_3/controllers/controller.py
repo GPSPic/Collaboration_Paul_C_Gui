eb1d8574fbb36ad5c105fd98e7922768193bf086
Paul Cumming <user@users-MacBook-Pro.local>
2023-03-15 15:03:51 +0000

    started extensions

--- flask_intro_week_3_day_3/controllers/controller.py
+++ flask_intro_week_3_day_3/controllers/controller.py
@@ -1,8 +1,12 @@
 from flask import render_template
 
 from app import app
-from models.order_list import orders
+from models.order_list import *
 
 @app.route('/')
 def index():
     return render_template('index.html', order_list=orders)
+
+@app.route('/order-1')
+def order_1():
+    return render_template('order-1.html', order1=order1)
